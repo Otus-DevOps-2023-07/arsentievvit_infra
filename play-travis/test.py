dd258d49e424e1b5f6a8c318b5016fc9c6ce01f3
Виталий Арсентьев <vit.arsentiev@gmail.com>
2023-08-10 22:39:54 +0300

    Fixed test.py

--- play-travis/test.py
+++ play-travis/test.py
@@ -2,7 +2,7 @@ import unittest
 
 class NumberTest(unittest.TestCase):
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(1 + 1, 2)
 
 if __name__ == "__main__":
     unittest.main()
