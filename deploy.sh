50e64c4474b7e7a47c29d650776252e97030ab0d
arsentievvit <vit.arsentiev@gmail.com>
2023-08-24 20:22:37 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt update -y && sudo apt install git -y
-git clone -b monolith https://github.com/express42/reddit.git cd ~/reddit
+git clone -b monolith https://github.com/express42/reddit.git
+cd ~/reddit
 [ -e /usr/bin/bundle ] && bundle install
 puma -d
-IP=`curl 2ip.ru`
-PORT=`ps aux | grep puma | grep -v grep | sed -n 's/.*:\([0-9]\+\).*/\1/p'`
-echo "You can connect to the app using:"
-echo "$IP:$PORT" | tee ~/connect.txt
