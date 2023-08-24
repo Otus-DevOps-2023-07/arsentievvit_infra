005817d44ad1faa854edc2136289e8dbebe947f0
arsentievvit <vit.arsentiev@gmail.com>
2023-08-24 20:26:05 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update -y && sudo apt install git -y
-git clone -b monolith https://github.com/express42/reddit.git
-cd ~/reddit
+git clone -b monolith https://github.com/express42/reddit.git reddit
+cd ./reddit
 [ -e /usr/bin/bundle ] && bundle install
 puma -d
