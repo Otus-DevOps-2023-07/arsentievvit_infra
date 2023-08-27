f047349480cf0b366ed19cb67bd67d833bd826a7
Виталий Арсентьев <vit.arsentiev@gmail.com>
2023-08-27 23:48:25 +0300

    Fixed errors, +immutable.pkr.hcl, +create-vm.sh

---
+++ packer/files/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+apt-get update -y && \
+apt-get install -y git ruby-full ruby-bundler build-essential mongodb
+git clone -b monolith https://github.com/express42/reddit.git /home/ubuntu/reddit
+cd /home/ubuntu/reddit && bundle install
