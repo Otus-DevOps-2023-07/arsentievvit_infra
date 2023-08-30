222ac7fc5d0e2656b4f7ed63e1d1e8a113985eec
Виталий Арсентьев <vit.arsentiev@gmail.com>
2023-08-30 20:05:19 +0300

    Fixed some more

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,3 @@
-
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.95.0"
-    }
-  }
-  required_version = "~> 1.5.1"
-}
-
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
