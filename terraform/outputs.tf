1c1d81de6fc18bd768ac5fbbfa9f54a7751560f1
Виталий Арсентьев <vit.arsentiev@gmail.com>
2023-09-06 10:02:37 +0300

    Deleted 2nd app output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,5 @@
 output "app-internal-ip" {
-  value = yandex_compute_instance.app[0].network_interface[0].ip_address
-}
-
-output "app2-internal-ip" {
-  value = yandex_compute_instance.app[1].network_interface[0].ip_address
+  value = yandex_compute_instance.app.network_interface[0].ip_address
 }
 
 output "bastion-host-ip" {
