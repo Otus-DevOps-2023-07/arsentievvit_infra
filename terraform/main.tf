d8ffe1d3a9e6c6142b6f8741aed598b43824de97
Виталий Арсентьев <vit.arsentiev@gmail.com>
2023-09-06 09:59:14 +0300

    Added network, subnet, fixed app count

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,17 @@ provider "yandex" {
   zone                     = var.zone
 }
 
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network"
+}
+
+resource "yandex_vpc_subnet" "app_subnet" {
+  name = "reddit-app-subnet"
+  zone = "ru-central1-a"
+  network_id = yandex_vpc_network.app-network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
 data "yandex_compute_image" "def-ubuntu" {
   family = "ubuntu-2004-lts"
 }
