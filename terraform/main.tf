f396754317cf51d4da8babd66f6cd777d47dce4b
Виталий Арсентьев <vit.arsentiev@gmail.com>
2023-09-10 22:05:32 +0300

    Updated README.md

--- terraform/main.tf
+++
@@ -1,6 +0,0 @@
-provider "yandex" {
-  service_account_key_file = var.service_account_key_file
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.zone
-}
