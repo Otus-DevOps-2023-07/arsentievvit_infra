19690a43fe5b9e69a5b20e37fd52b0d892cca1b1
Виталий Арсентьев <vit.arsentiev@gmail.com>
2023-09-22 14:38:15 +0300

    Fixed some

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -15,6 +15,31 @@ resource "yandex_vpc_default_security_group" "securitygroup" {
     v4_cidr_blocks = ["0.0.0.0/0"]
     port           = 22
   }
+
+  ingress {
+    protocol = "ICMP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Web Server"
+    port = 9292
+    protocol = "TCP"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MongoDB"
+    protocol = "TCP"
+    port = 27017
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol = "ANY"
+    description = "Allow outgoing"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "yandex_vpc_subnet" "subnet" {
